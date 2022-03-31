ce21241f93591b82a13914ccc44e2bbb598405d5
Keng Yang <kyang0908@gmail.com>
2022-03-30 20:50:41 -0400

    adding comments to js file

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const addButton = document.getElementById('addButton');
-addButton.addEventListener('click' , function(event){
-    let addItem = document.getElementById('addItem').value;
-    const addNewItem = document.createElement('li');
-    addNewItem.innerHTML = addItem;
-    const list = document.querySelector('ul');
-    list.appendChild(addNewItem);
-    document.getElementById('addItem').value = '';
+const addButton = document.getElementById('addButton'); //dom element to enter a new to-do
+addButton.addEventListener('click' , function(event){ //event listener function on the button to add a new to-do when button is clicked
+    let addItem = document.getElementById('addItem').value; // the value of addItem 
+    const addNewItem = document.createElement('li'); //creates html <li> tag
+    addNewItem.innerHTML = addItem; //sets element in <li> to addItem value
+    const list = document.querySelector('ul'); //gets the ul (parent value of <li>)
+    list.appendChild(addNewItem); ////add list item to the bototm of the list
+    document.getElementById('addItem').value = ''; //sets the value of addItem to a empty string
 
-    removeEventListener(addNewItem);
+    removeEventListener(addNewItem); //creates event listener for addNewItem
 })
 
-const removeItem = document.getElementsByTagName('li');
-for(let i = 0; i < removeItem.length; i++)
+const removeItem = document.getElementsByTagName('li'); //removes item from list when clicked on 
+for(let i = 0; i < removeItem.length; i++) //add removeEventListener to each <li> bc we can't bind it to getElementsByTagName
 {
-    removeEventListener(removeItem[i]);
+    removeEventListener(removeItem[i]); //add removeEventListener to each <li>
 }
 
-function removeEventListener(el)
+function removeEventListener(el) 
 {
-    el.addEventListener('click' , function(event){
-        event.target.classList.add('crossOut');
-        setTimeout(()=> {event.target.remove()}, 1000);
+    el.addEventListener('click' , function(event){ ////removes the <li> element
+        event.target.classList.add('crossOut'); //adds class crossOut, class will inherit crossOut css
+        setTimeout(()=> {event.target.remove()}, 1000); //when user clicks on list, item is crossed out and removes item after 1 second
     })
 }
 
